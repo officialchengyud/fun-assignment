9d19fb579935ee06e4b4899f77399388501c5f26
Cheng-Yu Dong <39908329+officialchengyud@users.noreply.github.com>
2025-05-11 21:17:24 -0400

    Setup packages and UI library

---
+++ src/Dashboard/index.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <h1>Token Price Explorer</h1>;
+}
+
+export default Dashboard;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HeroUIProvider } from "@heroui/react";
+import "./index.css";
+import Dashboard from "./Dashboard";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HeroUIProvider>
+      <Dashboard />
+    </HeroUIProvider>
+  </StrictMode>
+);
